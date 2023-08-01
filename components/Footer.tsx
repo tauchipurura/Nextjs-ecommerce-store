7156923481d173d20f1129053b6aeb299c34dbc0
tau chipurura <tauraichipurura@gmail.com>
2023-08-01 14:14:44 +0200

    nextjs final store

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return ( 
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-sm text-black">
+                    @copy: 2023 TauModelStore, Inc, all rights are reserved.
+
+                </p>
+
+            </div>
+
+        </footer>
+     );
+}
+ 
+export default Footer;
